3f5191de891b29465e3bc3f2b5dc15cc5cfc356d
john <john.mobiledev0403@gmail.com>
2017-06-20 18:24:21 +0530

    removed comments

--- app.js
+++ app.js
@@ -11,54 +11,8 @@ app.use(express.static(__dirname+'/public'));
 app.get('/', routes.home);
 app.get('/:page', routes.page);
 
-
-/*
-app.get('/', function(req, res){
-    res.render('home',{headTitle: 'We belive that every city have something to say', title: 'EJS Template', content: "Home content"});
-});
-
-
-app.get('/:page', function(req, res){
-   var headTitle, title, content;
-    title = "I Love My City";
-    
-    if(req.params.page == 'about-us'){
-        headTitle = 'AboutUs';
-        title = "About Us";
-        content = "We are in learning";
-    }
-     if(req.params.page == 'who-we-are'){
-        headTitle = 'Who We Are';
-        title = "Who we are";
-        content = "Who we are details";
-    }
-    if(req.params.page == 'what-we-have-done'){
-        headTitle = 'What we have done';
-        title = "What We Have Done";
-        content = "So far what we have done";
-    }
-    if(req.params.page == 'programmes'){
-        headTitle = 'Programmes';
-        title = "Programmes";
-        content = "Program details";
-    }
-    if(req.params.page == 'facilities-amenities'){
-        headTitle = 'Facilities & Amenties';
-        title = "Facilities & Amenties";
-        content = "Facilities & Amenties details";
-    }
-    if(req.params.page == 'admissions'){
-        headTitle = 'Admissions';
-        title = "Admissions";
-        content = "Admissions details";
-    }
-    
-    
-    res.render('page', {title: title, headTitle: headTitle, content: content});
-});
-
-*/
 var port = process.env.PORT || 8080;
+
 app.listen(port, function(){
    console.log("Server Running at port " + port); 
 });
